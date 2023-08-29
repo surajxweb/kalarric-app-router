e46fcbf1203486aa72e3bee6102d39c506cc06c5
Suraj Katyayan <katyayansuraj@gmail.com>
2023-08-29 09:51:54 +0530

    bug fix

--- app/store/wallets/[id]/page.tsx
+++ app/store/wallets/[id]/page.tsx
@@ -6,33 +6,100 @@ import styles from "@/app/store/ProductPage.module.css";
 import Offers from "@/components/Offers";
 import ProductImage from "@/components/ProductImage";
 import InfoTabs from "@/components/ProductInfoTabs";
+import { useState } from "react";
+
+interface Product {
+  productId: number;
+  productName: string;
+  category: string;
+  description: string;
+  productInfo: {
+    material: string;
+    origin: string;
+    weight: string;
+    dimension: string;
+  };
+  quantity: { name: string; number: number }[];
+  mrp: number;
+  price: number;
+  imageURL: string[];
+}
+
+interface Account {
+  rollNo: number;
+  name: string;
+  email: string;
+  number: number;
+  address: {
+    line1: string;
+    line2: string;
+    city: string;
+    pincode: number;
+    nation: string;
+  };
+  orderHistory: {
+    date: string;
+    totalPrice: number;
+    purchasedProducts: { productId: number; pricePaid: number }[];
+  }[];
+}
+
+interface Database {
+  products: Product[];
+  accounts: Account[];
+}
 
 // Define the type for the Next.js page component props
-interface ProductProductPageProps {
+interface WalletProductPageProps {
   params: {
     id: string;
   };
 }
 
-const WalletProductPage = ({ params }: { params: { id: string } }) => {
-  const product = database.products.filter(
+const WalletProductPage: React.FC<WalletProductPageProps> = ({ params }) => {
+  const [selectedSize, setSelectedSize] = useState<string | null>(null);
+  const [isAddedToBag, setIsAddedToBag] = useState(false);
+  const [sizeError, setSizeError] = useState(false);
+
+  const product: Product[] = database.products.filter(
     (product) =>
       product.productId === parseInt(params.id) &&
       product.category === "wallets"
   );
 
-  const objectWithStocks = product[0].quantity.filter(
-    (size: any) => size.number > 0
-  );
+  const objectWithStocks: { name: string; number: number }[] | undefined =
+    product[0]?.quantity?.filter((size) => size.number > 0);
+
+  const sizeSelectHandler = (size: string) => {
+    setSelectedSize(size);
+    setSizeError(false);
+    setIsAddedToBag(false);
+  };
+
+  const addToCartHandler = () => {
+    //add to cart
+    selectedSize ? setIsAddedToBag(true) : setSizeError(true);
+  };
 
   return (
     <>
       <Offers />
-      {product.length > 0 ? (
+      {product?.length > 0 ? (
         <div className={styles.container}>
           <div className={styles.productContainer}>
             <div className={styles.imageContainer}>
-              <ProductImage imageURLs={product[0].imageURL} />
+              <div className={styles.images}>
+                <ProductImage
+                  thumbnails={"left"}
+                  imageURLs={product[0].imageURL}
+                />
+              </div>
+              <div className={styles.mobile_images}>
+                <ProductImage
+                  thumbnails={"bottom"}
+                  imageURLs={product[0].imageURL}
+                />
+              </div>
             </div>
             <div className={styles.infoContainer}>
               <Link href={`/store/${product[0].category}`}>
@@ -43,16 +110,41 @@ const WalletProductPage = ({ params }: { params: { id: string } }) => {
               <div className={styles.sizesRow}>
                 <div className={styles.select}>Select Size</div>
                 <div className={styles.sizes}>
-                  {objectWithStocks.map((size) => (
-                    <button key={size.name}>{size.name}</button>
+                  {objectWithStocks?.map((size) => (
+                    <button
+                      onClick={() => sizeSelectHandler(size.name)}
+                      className={styles.sizeButton}
+                      style={{
+                        backgroundColor:
+                          selectedSize === size.name ? "black" : "white",
+                        color: selectedSize === size.name ? "white" : "black",
+                      }}
+                      key={size.name}
+                    >
+                      {size.name}
+                    </button>
                   ))}
                 </div>
+                {sizeError && (
+                  <div className={styles.sizeError}>
+                    Select a valid size. We&apos;re not good at guessing.
+                  </div>
+                )}
               </div>
               <div className={styles.prices}>
                 <div className={styles.price}>{`₹ ${product[0].price}`}</div>
                 <div className={styles.mrp}>{`MRP ${product[0].mrp}`}</div>
               </div>
-              <button className={styles.button}>ADD TO CART</button>
+              <button onClick={addToCartHandler} className={styles.button}>
+                ADD TO CART
+              </button>
+              {isAddedToBag && (
+                <Link href={"/cart"}>
+                  <div className={styles.roadToCheckout}>
+                    Added to cart. Click here to checkout.
+                  </div>
+                </Link>
+              )}
               <InfoTabs info={product[0].productInfo} />
             </div>
           </div>
