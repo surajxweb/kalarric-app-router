8f2107bcbdc317ac589aeae70dd8e0d8f342e7c5
Suraj Katyayan <katyayansuraj@gmail.com>
2023-10-30 10:01:17 +0530

    test initialize

--- components/Offers.tsx
+++ components/Offers.tsx
@@ -5,8 +5,7 @@ export default function Offers() {
   return (
     <div className={styles.nav2}>
       <div className={styles.text}>
-        <span className={styles.vibratingSpan}>⚡</span>Festival Extravaganza
-        Sale is now live. Free delivery on all prepaid orders over ₹ 999.
+        <span className={styles.vibratingSpan}>⚡</span>This is a test
         <span className={styles.vibratingSpan}>⚡</span>
       </div>
     </div>
